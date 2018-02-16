bc65f9943f4ffec210986885a3729458ad72a998
rendoir <up201503822@fe.up.pt>
2018-02-16 21:22:14 +0000

    All fixed! Now working. Needs refactor.

--- src/com/client/Client.java
+++ src/com/client/Client.java
@@ -4,44 +4,22 @@ import java.io.IOException;
 import java.net.*;
 
 public class Client {
-    static int PORT = 5000;
-
     private InetAddress address;
     private byte[] dataBuffer;
 
     private DatagramSocket socket;
     private DatagramPacket packet;
 
-    public Client(String hostname, String data) {
-        this.setHostname(hostname);
-        this.setData(data);
+    public Client(String hostname, int port, String data) {
         try{
-            socket = new DatagramSocket(PORT, address);
-        } catch (SocketException e) {
-            e.printStackTrace();
-        }
-        this.createPackage();
-        try {
+            socket = new DatagramSocket();
+            address = InetAddress.getByName(hostname);
+            dataBuffer = data.getBytes();
+            packet = new DatagramPacket(dataBuffer, dataBuffer.length, address, port);
             System.out.println("Sending: " + new String(packet.getData()));
             socket.send(packet);
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    private void setHostname(String hostname){
-        try {
-            address = InetAddress.getByName(hostname);
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void setData(String data) {
-        dataBuffer = data.getBytes();
-    }
-
-    private void createPackage(){
-        this.packet = new DatagramPacket(dataBuffer, dataBuffer.length, address, PORT);
-    }
+      }
 }
--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -6,9 +6,9 @@ import com.server.Server;
 public class Main {
 
     public static void main(String[] args) {
-        if (args.length == 3 && (args[0].equals("--client") || args[0].equals("-C"))) {
+        if (args.length == 4 && (args[0].equals("--client") || args[0].equals("-C"))) {
             System.out.println("Initializing Client");
-            Client client = new Client(args[1], args[2]);
+            Client client = new Client(args[1], Integer.parseInt(args[2]), args[3]);
         } else if (args.length == 2 && (args[0].equals("--server") || args[0].equals("-S"))) {
             System.out.println("Initializing Server");
             Server server = new Server(Integer.parseInt(args[1]));
