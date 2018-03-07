298dc1394d710ab9abf36a5c1a36c8ebca22a85b
João Carvalho <joaocarvalho9@gmail.com>
2018-03-07 12:59:51 +0000

    continue last commit

--- src/com/DBS/Main.java
+++ src/com/DBS/Main.java
@@ -1,8 +1,10 @@
 package com.DBS;
 
+import com.DBS.Message.Message;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(Message.encode(Message.parse("PUTCHUNK 1 0 mlksjhgwdjbjdcksfjksdnxmnsjkcbhdgvsbm 0 1 \r\n\r\n,ljduwyqghw1ib2qjm   snkdlsa nsanckl s,a csalc sac san@ieohdubwksckqn")));
     }
 }
