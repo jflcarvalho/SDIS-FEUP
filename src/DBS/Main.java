5bb82111e327389992dae2f34a259399dee97102
João Carvalho <joaocarvalho9@gmail.com>
2018-03-22 20:28:30 +0000

    knslja

--- src/DBS/Main.java
+++ src/DBS/Main.java
@@ -1,6 +1,5 @@
 package DBS;
 
-import DBS.Message.Message;
 import DBS.Protocol.Backup;
 
 import java.net.SocketException;
@@ -10,8 +9,6 @@ import static DBS.Utils.Constants.testFilePath;
 public class Main {
 
     public static void main(String[] args) throws SocketException {
-        System.out.println(Message.encode(Message.parse("PUTCHUNK 1 0 mlksjhgwdjbjdcksfjksdnxmnsjkcbhdgvsbm 0 1 \r\n\r\n,ljduwyqghw1ib2qjm   snkdlsa nsanckl s,a csalc sac san@ieohdubwksckqn")));
-
         Backup backup = new Backup(testFilePath, 1);
         backup.run();
     }
--- src/DBS/Protocol/Backup.java
+++ src/DBS/Protocol/Backup.java
@@ -19,15 +19,19 @@ public class Backup implements Protocol {
     public Backup(String file_path, int replication_degree) {
         this.file_path = file_path;
         this.replication_degree = replication_degree;
-        mFile = new M_File(file_path);
         peer = new Peer();
     }
 
     public void run() {
-      System.out.println("Backing up " + file_path);
-      System.out.println("Replication degree: " + replication_degree);
-      System.out.println(mFile.toString());
-      storeChunk(mFile.getChunks()[0]);
+        System.out.println("\n----------------- BACKUP --------------------\n");
+        System.out.println("Backing up " + file_path);
+        System.out.println("Replication degree: " + replication_degree + "\n");
+        mFile = new M_File(file_path);
+        System.out.println(mFile.toString());
+        for(int i = 0; i < mFile.getChunks().length; i++){
+            storeChunk(mFile.getChunks()[i]);
+        }
+        System.out.println("\n------------------ END ---------------------\n");
     }
 
     public void sendChunk(Chunk chunk) {
@@ -40,5 +44,9 @@ public class Backup implements Protocol {
         String file_path = "backup/" + peer.getPeerID() + "/" + chunk.getFileID() + "/" + chunk.getChunkID();
         long file_Size = (new File(file_path)).length();
         peer.addChunk(chunk, file_Size);
+    }
+
+    public Chunk readChunk(){
+        return null;
     }
 }
