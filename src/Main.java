f9200fa72f8e2a026b13cee98a7dc11e69098023
João Carvalho <joaocarvalho9@gmail.com>
2018-05-23 19:44:00 +0100

    fix main

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,9 @@ import static utils.Constants.MENU_TITLE;
 
 public class Main {
     public static void main(String[] args){
-        //System.out.print(MENU_TITLE);
-        //String username;
-        //username = UI_Login.authMenu();
-        //System.out.println(username);
-
-        System.out.println((int) (Integer.toUnsignedLong(-100) - Integer.toUnsignedLong(-120)));
+        System.out.print(MENU_TITLE);
+        String username;
+        username = UI_Login.authMenu();
+        System.out.println(username);
     }
 }
