a8baaf8c38520b2b3130c9c8b9b92e42714127d7
João Seixas <jnuno1997@gmail.com>
2018-05-26 22:55:22 +0100

    Fixed a lot of stuff in ssl

--- client.sh
+++
@@ -1,2 +0,0 @@
-cd bin/
-java -Djavax.net.ssl.keyStore=../client.keys -Djavax.net.ssl.keyStorePassword=123456 -Djavax.net.ssl.trustStore=../truststore -Djavax.net.ssl.trustStorePassword=123456 SSLClient localhost 8080 SSL_RSA_WITH_NULL_MD5
