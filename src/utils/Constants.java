64a1957f5bbaeb415d49a4672b863ecd57ad02ac
João Carvalho <joaocarvalho9@gmail.com>
2018-05-28 03:41:15 +0100

    start get task data

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -28,8 +28,8 @@ public abstract class Constants {
             "0) Exit      \n";
 
     public static final String MENU_USER_AREA =
-            "1) Add peers.Task                \n" +
-            "2) Delete Pending peers.Task     \n" +
+            "1) Add Task                \n" +
+            "2) Delete Pending Task     \n" +
             "3) Consult All Tasks   \n\n" +
             "0) Exit  \n";
 
